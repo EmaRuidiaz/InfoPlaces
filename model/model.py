081663aa55554162c5ee16225eee6d01880ff47b
EmaRuidiaz <ema_ruidiaz@hotmail.com>
2019-06-08 15:36:34 -0300

    Agregué las clases a model

---
+++ model/model.py
@@ -0,0 +1,89 @@
+
+
+
+class User:
+	def __init__(self):
+		pass
+
+	def register(self):
+		pass
+
+
+
+class RegisteredUser(User):
+	def __init__(self):
+		User.__init__(self)
+
+		self.firstame = ""
+		self.lastname = ""
+		self.username = ""
+		self.birthdate = ""
+		self.email = ""
+		self.password = ""
+
+	def addtofavorites(self):
+		pass
+
+	def comment(self):
+		pass
+
+	def rate(self):
+		pass
+
+class ShopOwner(RegisteredUser):
+	def __init__(self):
+		RegisteredUser.__init__(self)
+
+	def answer(self):
+		pass
+
+	def modify(self):
+		pass
+
+	def createshop(self):
+		pass
+
+class Administrator(ShopOwner):
+	def __init__(self):
+		ShopOwner.__init__(self)
+
+	def createaccount(self):
+		pass
+
+class Type:
+	def __init__(self):
+		self.description = ""
+
+class Shop:
+	def __init__(self):
+		self.name = ""
+		self.phone = ""
+		self.streetname = ""
+		self.streetnumber = ""
+
+	def search(self):
+		pass
+
+class Commet:
+	def __init__(self):
+		self.date = ""
+
+class Rating:
+	def __init__(self):
+		self.number
+
+class Favorite:
+	def __init__(self):
+		pass
+
+class Answer:
+	def __init__(self):
+		pass
+
+class Schedule:
+	def __init__(self):
+		self.day = ""
+		self.turn = ""
+		self.opening = ""
+		self.closing = ""
+

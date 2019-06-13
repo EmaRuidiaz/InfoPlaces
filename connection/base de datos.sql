7cf49ac6861fcfb6eac020029bc47e00b2f33147
Agustin <agustin.fredes@outlook.com.ar>
2019-06-13 12:19:06 -0300

    base de datos agregada

---
+++ connection/base de datos.sql
@@ -0,0 +1,45 @@
+create database InfoPlaces
+use InfoPlaces
+
+create table PERSON (
+id int primary key auto_increment,
+`type` int(1) not null,
+first_name varchar(20) not null,
+last_name varchar(20) not null,
+email varchar(30) not null,
+`password` varchar(20) not null)
+
+create table SHOP (
+id int primary key auto_increment,
+`name` varchar(20) not null,
+street_name varchar(30) not null,
+street_num int(6),
+phone_num int(13),
+id_person int not null,
+foreign key (id_person) references PERSON(id))
+
+create table `COMMENT` (
+id int primary key auto_increment,
+content varchar(150) not null,
+`datetime` datetime not null,
+id_person int not null,
+foreign key (id_person) references PERSON(id),
+id_shop int not null,
+foreign key (id_shop) references SHOP(id))
+
+create table ANSWER (
+id int primary key auto_increment,
+content varchar(150) not null,
+`datetime` datetime not null,
+id_person int not null,
+foreign key (id_person) references PERSON(id),
+id_comment int not null,
+foreign key (id_comment) references `COMMENT`(id))
+
+create table RATING (
+id int primary key auto_increment,
+id_person int,
+foreign key (id_person) references PERSON(id),
+id_shop int,
+foreign key (id_shop) references SHOP(id),
+rating int)
